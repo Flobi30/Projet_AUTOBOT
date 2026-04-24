da582a80d99e28aa1ced4ca613c4a27802cd4ec0
Flobi30 <florent.bastide@yahoo.fr>
2026-04-24 06:51:32 +0200

    fix: update server IP to 204.168.251.201 in Analytics.tsx

--- Analytics.tsx
+++ Analytics.tsx
@@ -3,7 +3,7 @@ import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContai
 import MetricCard from '../components/ui/MetricCard';
 import { PieChart, Target, TrendingUp, Shield, BarChart3, Loader } from 'lucide-react';
 
-const API_BASE_URL = 'http://204.168.205.73:8080';
+const API_BASE_URL = 'http://204.168.251.201:8080';
 const API_TOKEN = 'autobot_token_12345';
 const authHeaders = { 'Authorization': `Bearer ${API_TOKEN}` };
 
